2f46fc89e02789dc5b26b061bf1f94a6cb7e66c0
Wilmer A. Rubio Rojas <rubiorojasw@gmail.com>
2018-11-28 10:10:55 -0500

    Updates

---
+++ precache-manifest.b8eee6a2b3791069306d99f9e0c1c0b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9517083e278f363f84f2",
+    "url": "/movilizame/static/js/runtime~main.9517083e.js"
+  },
+  {
+    "revision": "4d66ab7a55a6ebac08cd",
+    "url": "/movilizame/static/js/main.4d66ab7a.chunk.js"
+  },
+  {
+    "revision": "ecc0c0c307aa8b3a90b2",
+    "url": "/movilizame/static/js/1.ecc0c0c3.chunk.js"
+  },
+  {
+    "revision": "4d66ab7a55a6ebac08cd",
+    "url": "/movilizame/static/css/main.01b7756c.chunk.css"
+  },
+  {
+    "revision": "e771755c98c8b062e39638373a347492",
+    "url": "/movilizame/index.html"
+  }
+];
